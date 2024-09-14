5ba4c6ff0130bcd2394d430e309c4e3b1c7df5fc
Abolaji2K21 <151364721+Abolaji2K21@users.noreply.github.com>
2024-09-14 20:06:33 +0100

    Omo

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,47 +1,46 @@
 import React from 'react';
-import '@iconscout/unicons/css/line.css';
-import './index.module.css';
+import styles from './index.module.css'; // Importing as styles for CSS Modules
 
 const Navbar = () => {
     return (
         <>
-            <header className="header">
-                <nav className="nav container">
-                    <a href="index.html" className="nav_logo"> Abolaji</a>
+            <header className={styles.header}>
+                <nav className={styles.nav + " container"}>
+                    <a href="index.html" className={styles.nav_logo}> Abolaji</a>
 
-                    <div className="nav__menu">
-                        <ul className="nav__list grid">
-                            <li className="nav__item">
-                                <a href="#home" className="nav__link">
+                    <div className={styles.nav__menu}>
+                        <ul className={styles.nav__list + " grid"}>
+                            <li className={styles.nav__item}>
+                                <a href="#home" className={styles.nav__link}>
                                     <i className="uil uil-estate nav_icon"></i> Home
                                 </a>
                             </li>
-                            <li className="nav__item">
-                                <a href="#about" className="nav__link">
+                            <li className={styles.nav__item}>
+                                <a href="#about" className={styles.nav__link}>
                                     <i className="uil uil-user nav_icon"></i> About
                                 </a>
                             </li>
 
-                            <li className="nav__item">
-                                <a href="#skills" className="nav__link">
+                            <li className={styles.nav__item}>
+                                <a href="#skills" className={styles.nav__link}>
                                     <i className="uil uil-file-alt nav_icon"></i> Skills
                                 </a>
                             </li>
 
-                            <li className="nav__item">
-                                <a href="#services" className="nav__link">
+                            <li className={styles.nav__item}>
+                                <a href="#services" className={styles.nav__link}>
                                     <i className="uil uil-briefcase-alt nav_icon"></i> Services
                                 </a>
                             </li>
 
-                            <li className="nav__item">
-                                <a href="#portfolio" className="nav__link">
+                            <li className={styles.nav__item}>
+                                <a href="#portfolio" className={styles.nav__link}>
                                     <i className="uil uil-scenery nav_icon"></i> Portfolio
                                 </a>
                             </li>
 
-                            <li className="nav__item">
-                                <a href="#contact" className="nav__link">
+                            <li className={styles.nav__item}>
+                                <a href="#contact" className={styles.nav__link}>
                                     <i className="uil uil-message nav_icon"></i> Contact
                                 </a>
                             </li>
@@ -50,7 +49,7 @@ const Navbar = () => {
                         <i className="uil uil-times nav_close"></i>
                     </div>
 
-                    <div className="nav__toggle">
+                    <div className={styles.nav__toggle}>
                         <i className="uil uil-apps nav_icon"></i>
                     </div>
                 </nav>
