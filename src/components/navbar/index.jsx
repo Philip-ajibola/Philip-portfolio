4ae0c2061f29d355b4ac3bec4b20c20073af64fd
Abolaji2K21 <151364721+Abolaji2K21@users.noreply.github.com>
2024-09-14 22:33:44 +0100

    StillTryingToFixMyNavbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,56 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.css'; // Importing as styles for CSS Modules
 
 const Navbar = () => {
+    const [activeNav, setActiveNav] = useState('#home');
+
     return (
         <>
             <header className={styles.header}>
-                <nav className={styles.nav + " container"}>
-                    <a href="index.html" className={styles.nav_logo}> Abolaji</a>
+                <nav className={`${styles.nav} container`}>
+                    <a href="index.html" className={styles.nav_logo}>Abolaji</a>
 
                     <div className={styles.nav__menu}>
-                        <ul className={styles.nav__list + " grid"}>
+                        <ul className={`${styles.nav__list} grid`}>
                             <li className={styles.nav__item}>
-                                <a href="#home" className={styles.nav__link}>
-                                    <i className="uil uil-estate nav_icon"></i> Home
-                                </a>
-                            </li>
-                            <li className={styles.nav__item}>
-                                <a href="#about" className={styles.nav__link}>
-                                    <i className="uil uil-user nav_icon"></i> About
-                                </a>
-                            </li>
-
-                            <li className={styles.nav__item}>
-                                <a href="#skills" className={styles.nav__link}>
-                                    <i className="uil uil-file-alt nav_icon"></i> Skills
+                                <a
+                                    href="#home"
+                                    className={`${styles.nav__link} ${activeNav === '#home' ? styles.active_link : ''}`}
+                                    onClick={() => setActiveNav('#home')}
+                                >
+                                    <i className={`uil uil-estate ${styles.nav__icon}`}></i> Home
                                 </a>
                             </li>
 
                             <li className={styles.nav__item}>
-                                <a href="#services" className={styles.nav__link}>
-                                    <i className="uil uil-briefcase-alt nav_icon"></i> Services
+                                <a
+                                    href="#about"
+                                    className={`${styles.nav__link} ${activeNav === '#about' ? styles.active_link : ''}`}
+                                    onClick={() => setActiveNav('#about')}
+                                >
+                                    <i className={`uil uil-user ${styles.nav__icon}`}></i> About
                                 </a>
                             </li>
 
                             <li className={styles.nav__item}>
-                                <a href="#portfolio" className={styles.nav__link}>
-                                    <i className="uil uil-scenery nav_icon"></i> Portfolio
+                                <a
+                                    href="#skills"
+                                    className={`${styles.nav__link} ${activeNav === '#skills' ? styles.active_link : ''}`}
+                                    onClick={() => setActiveNav('#skills')}
+                                >
+                                    <i className={`uil uil-file-alt ${styles.nav__icon}`}></i> Skills
                                 </a>
                             </li>
 
                             <li className={styles.nav__item}>
-                                <a href="#contact" className={styles.nav__link}>
-                                    <i className="uil uil-message nav_icon"></i> Contact
+                                <a
+                                    href="#services"
+                                    className={`${styles.nav__link} ${activeNav === '#services' ? styles.active_link : ''}`}
+                                    onClick={() => setActiveNav('#services')}
+                                >
+                                    <i className={`uil uil-briefcase-alt ${styles.nav__icon}`}></i> Services
+                                </a>
+                            </li>
+
+                            <li className={styles.nav__item}>
+                                <a
+                                    href="#portfolio"
+                                    className={`${styles.nav__link} ${activeNav === '#portfolio' ? styles.active_link : ''}`}
+                                    onClick={() => setActiveNav('#portfolio')}
+                                >
+                                    <i className={`uil uil-scenery ${styles.nav__icon}`}></i> Portfolio
+                                </a>
+                            </li>
+
+                            <li className={styles.nav__item}>
+                                <a
+                                    href="#contact"
+                                    className={`${styles.nav__link} ${activeNav === '#contact' ? styles.active_link : ''}`}
+                                    onClick={() => setActiveNav('#contact')}
+                                >
+                                    <i className={`uil uil-message ${styles.nav__icon}`}></i> Contact
                                 </a>
                             </li>
                         </ul>
 
-                        <i className={`uil uil-times ${styles.nav__close}`}></i>
+                        <i className={`uil uil-times ${styles.nav__icon} ${styles.nav__close}`}></i>
                     </div>
 
                     <div className={styles.nav__toggle}>
-                        <i className="uil uil-apps nav_icon"></i>
+                        <i className={`uil uil-apps ${styles.nav__icon}`}></i>
                     </div>
                 </nav>
             </header>
